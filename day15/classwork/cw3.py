2c95be3dbe3f0793492d7bba50adefc7a444d2ad
andria khabeishvili <andriakhabeishvili21@gmail.com>
2026-01-31 12:04:41 +0400

    1

---
+++ day15/classwork/cw3.py
@@ -0,0 +1,10 @@
+# 3)მომხარებელს შემოატანინე  ერთი ინტეჯერი, და შექმენით ცვლადი სადაც შეინახება სტრიინგი, შემდეგ 0 ინდექსიდან მომხარებლის შემოტანილი რიცხვის  ინდექსამდე გამოიტანეთ ყველა ასო
+# მაგ:  input1: 6
+# string = 'aleksandre'
+
+# output:  'aleksa'
+
+name='andria'
+num=int(input('enter a random number (1-6 recomended)'))
+num1=0
+print(name[num1:num])
