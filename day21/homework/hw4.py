d9aeae8a538fafb291d048de059f46f5025efbf8
andria khabeishvili <andriakhabeishvili21@gmail.com>
2026-03-16 00:16:55 +0400

    1

---
+++ day21/homework/hw4.py
@@ -0,0 +1,6 @@
+
+#4)შექმენით ფუნქცია რომელსაც ერქმევა თქვენი საყვარელი მანაქნის სახელი,ფუნქციას უნდა გადაცეთ ბრენდი,ცხენის ძალა , sport car. ამის შემდეგ ეს ინფორმაციები გამოიტანეთ ეკრანზე
+
+def koneiseggREGERA(brand,hp,sport car):
+    print("koneigsegg regera is made by" + brand + "it has " + hp "horse power " + "and it is a " + sport car)
+    koneiseggREGERA("koneisegg", "1600" , "sports car")
---
+++ day21/homework/hw7.py
@@ -0,0 +1,6 @@
+#  7)შექმენი ფუნქცია Find_letter,სადაც გადაცემ სტრინგს.შენი დავალება არის რომ ამ სტრინგიდან დაითვალო რამდენი a ურევია მასში და გამოიტანე ეკრანზე
+
+string=input("enter a random string : ")
+
+def find_letter(string):
+    print(string.find("a"))
